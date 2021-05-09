827c80269bc7d2671937614426b4fa5e7f078583
aybaris <83760836+aybaris@users.noreply.github.com>
2021-05-09 17:23:22 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+                CarManager carManager = new CarManager(new EfCarDal());
+
+                foreach (var car in carManager.GetAll())
+                {
+                    Console.WriteLine("Marka Bilgi : "+car.Description);
+                    Console.WriteLine("Günlük fiyatı: "+ car.DailyPrice);
+                    Console.WriteLine("Model yılı : " +car.ModelYear);
+
+
+               
+        }
+
+          
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,72 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car {Id = 1 , BrandId = 1, ColorId=1 , DailyPrice = 250000 , ModelYear = 2021, Description = "Opel Corsa - Beyaz - Yeni Kasa"},
+                new Car {Id = 2 , BrandId = 2, ColorId=2 , DailyPrice = 158000 , ModelYear = 2021, Description = "Renault Kangoo - Füme Gri - Yeni Kasa"},
+                new Car {Id = 3 , BrandId = 2, ColorId=1 , DailyPrice = 129999 , ModelYear = 2021, Description = "Renault Dacia Logan - Beyaz - Yeni Kasa"},
+                new Car {Id = 4 , BrandId = 3, ColorId=3 , DailyPrice = 548000 , ModelYear = 2021, Description = "Volvo - Yeşil  - Yeni Kasa"},
+                new Car {Id = 5 , BrandId = 3, ColorId=4 , DailyPrice = 453700 , ModelYear = 2021, Description = "Volvo - Siyah - Yeni Kasa"},
+            };
+
+        }
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=> c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int BrandId)
+        {
+            return _cars.Where(c => c.BrandId == BrandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+
+        }
+    }
+}
